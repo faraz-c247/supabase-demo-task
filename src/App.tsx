a41ee266e403021e515d055c73d7f3631fe2a1bd
faraz-c247 <faraz.k@chapter247.com>
2025-11-11 11:20:29 +0530

    initial migration setup with new db

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,7 @@
-import { useState } from 'react'
-import { useAuth } from './hooks/useAuth'
-import { useTodos } from './hooks/useTodos'
-import { useProfile } from './hooks/useProfile'
-
-// Components
-import Layout from './components/Layout'
-import AuthForm from './modules/auth/AuthForm'
-import TodoList from './modules/todos/TodoList'
-import ProfileView from './modules/profile/ProfileView'
-import ProfileEdit from './modules/profile/ProfileEdit'
-
 import './App.css'
 
 export default function App() {
-  const { user, loading: authLoading, signIn, signUp, signOut } = useAuth()
-  const { todos, loading: todosLoading, addTodo, toggleComplete, deleteTodo } = useTodos(user)
-  const { profile, loading: profileLoading, updateProfile, uploadAvatar, deleteAvatar } = useProfile(user)
-  
-  const [isEditingProfile, setIsEditingProfile] = useState(false)
-
-  // Show loading state while checking auth
-  if (authLoading) {
-    return (
-      <div className="loading-screen">
-        <div className="spinner"></div>
-        <p>Loading...</p>
-      </div>
-    )
-  }
-
-  // Show auth form if not logged in
-  if (!user) {
-    return <AuthForm onSignIn={signIn} onSignUp={signUp} />
-  }
-
-  // Main app with modular layout
   return (
-    <Layout 
-      userName={profile?.full_name || user.email || 'User'} 
-      onSignOut={signOut}
-    >
-      {/* Todos Module */}
-      <div data-tab-id="todos">
-        <TodoList
-          todos={todos}
-          loading={todosLoading}
-          onAdd={addTodo}
-          onToggle={toggleComplete}
-          onDelete={deleteTodo}
-        />
-      </div>
-
-      {/* Profile Module */}
-      <div data-tab-id="profile">
-        {isEditingProfile ? (
-          <ProfileEdit
-            profile={profile}
-            onSave={updateProfile}
-            onUploadAvatar={uploadAvatar}
-            onDeleteAvatar={deleteAvatar}
-            onCancel={() => setIsEditingProfile(false)}
-          />
-        ) : (
-          <ProfileView
-            profile={profile}
-            loading={profileLoading}
-            userEmail={user.email || ''}
-            onEdit={() => setIsEditingProfile(true)}
-          />
-        )}
-      </div>
-    </Layout>
+    <p>Hello World</p>
   )
 }
--- src/modules/todos/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as TodoList } from './TodoList'
-export { default as TodoItem } from './TodoItem'
-export { default as AddTodo } from './AddTodo'
